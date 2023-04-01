affd9f1f48360c79e3037a6e9e4f85aa70691513
russgards03 <99627318+russgards03@users.noreply.github.com>
2023-04-01 08:15:29 +0800

    Add files via upload

---
+++ receivecrud/products-module/update-products.php
@@ -0,0 +1,36 @@
+<h3>Product Details</h3>
+<div id="form-block">
+
+    <form method="POST" action="processes/process.product.php?action=update">
+        <div id="form-block-half">
+            <label for="product_name">Product Name</label>
+            <input type="text" id="product_name" class="input" name="product_name" value="<?php echo $product->get_product_name($id);?>" placeholder="Name of product..">
+
+            <label for="product_price">Product Type</label>
+            <select id="type" name="type">
+              <option value="bread" <?php if($product->get_product_type($id) == "bread"){ echo "selected";};?>>Bread</option>
+            </select>
+
+            <label for="product_price">Product Price</label>
+            <input type="text" id="product_price" class="input" name="product_price" value="<?php echo $product->get_product_price($id);?>" placeholder="Price of product..">
+
+            <input type="hidden" id="product_id" class="input" name="product_id" value="<?php echo $id;?>">
+
+        </div>
+            
+            <div id="button-block">
+            <input type="submit" value="Update">
+                
+        </div>
+    </form> 
+
+    <form action="processes/process.product.php?action=delete" method="POST">
+    <input type="hidden" name="product_id" value="<?php echo $id; ?>">
+    <div id="button-block">
+      <input type="submit" value="Delete Product">
+    </div>
+    </form>
+
+
+
+</div>
---
+++ usercrud/users-products-module/index.php
@@ -0,0 +1,15 @@
+<div id="second-submenu">
+    <a href="index.php?page=usersproducts&subpage=users">Users</a> |
+</div>
+<div id="content">
+    <?php
+      switch($subpage){
+                case 'users':
+                    require_once 'users-module/index.php';
+                break; 
+                default:
+                    require_once 'main.php';
+                break; 
+            }
+    ?>
+  </div>
